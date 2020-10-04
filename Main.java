84b14e834a9405f0432f32f79dc211507e1b522f
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 12:17:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Find out what animal the user is describing
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,39 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // asks the user if the animal has feathers
+    System.out.println("Does the animal have feathers? (yes/no)");
+    String feathersAnswer = input.nextLine();
+
+    if(feathersAnswer.equals("yes")){
+      System.out.println("Does the animal swim? (yes/no)");
+      String swimsAnswer = input.nextLine();
+
+      // determine the type of bird
+      if(swimsAnswer.equals("yes")){
+        System.out.println("This is a duck");
+      } else if(swimsAnswer.equals("no")){
+        System.out.println("This is a hen");
+      }
+
+      // if the user answered no
+    } else {
+      System.out.println("Does the animal have legs? (yes/no)");
+      String legsAnswer = input.nextLine();
+
+      // determine the animal
+      if(legsAnswer.equals("yes")){
+        System.out.println("This is a lizard");
+      } else if(legsAnswer.equals("no")){
+        System.out.println("This is a snake");
+      }
+
+
+
+    }
+
   }
 }
